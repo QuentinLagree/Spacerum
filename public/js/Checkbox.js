6a0947863046d74735de9d1201d5332b6efbd81f
lagreequentindev21@gmail.com <unconfigured@null.spigotmc.org>
2023-01-24 09:50:24 +0100

    Try Eye in password field + modify CSS

--- public/js/Checkbox.js
+++ public/js/Checkbox.js
@@ -1,13 +1,8 @@
 const checked = document.querySelector(".checked");
 const notChecked = document.querySelector(".not-checked");
-const checkboxContain = document.querySelector(".checkbox")
+const checkboxContain = document.querySelector(".remember-me")
 const checkbox = document.querySelector("#checkbox")
-checked.addEventListener('click', (e) => {
+checkboxContain.addEventListener('click', (e) => {
     checkbox.checked = false;
-    checkboxContain.classList.remove("checked");
-})
-
-notChecked.addEventListener('click', (e) => {
-    checkbox.checked = true;
-    checkboxContain.classList.add("checked")
+    checkboxContain.classList.toggle("checked");
 })
